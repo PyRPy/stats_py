9cb3a6b3d09921bed4608e092ae7a70dca0b88ff
PyRPy <44626163+PyRPy@users.noreply.github.com>
2019-11-28 10:43:20 -0600

    bouncing ball - step by step

---
+++ py4kids/13_bouncing_ball2.py
@@ -0,0 +1,50 @@
+from tkinter import *
+import random
+import time
+
+## create the ball class
+class Ball:
+    def __init__(self, canvas, color):
+        self.canvas = canvas
+        self.id = canvas.create_oval(10,10,25,25, fill=color)
+        self.canvas.move(self.id, 245, 100)
+        starts = [-3, -2, -1, 1, 2, 3]
+        random.shuffle(starts)
+        self.x = starts[0]
+        self.y = -3
+        self.canvas_height = self.canvas.winfo_height()
+        self.canvas_width = self.canvas.winfo_width()
+
+    def draw(self):
+        self.canvas.move(self.id, self.x, self.y)
+        pos = self.canvas.coords(self.id)
+        if pos[1] <=  0:
+            self.y = 3
+        if pos[3] >= self.canvas_height:
+            self.y =  -3
+        if pos[0] <= 0:
+            self.x = 3
+        if pos[2] >= self.canvas_width:
+            self.x = -3
+        
+
+
+## create game canvas
+tk = Tk()
+tk.title("Game")
+tk.resizable(0,0)
+tk.wm_attributes("-topmost", 1)
+canvas = Canvas(tk, width=500, height=400, bd=0, highlightthickness=0)
+canvas.pack()
+tk.update()
+
+ball = Ball(canvas, 'red')
+
+while 1:
+    ball.draw()
+    tk.update_idletasks()
+    tk.update()
+    time.sleep(0.01)
+
+## making the ball bounce
+
