26499cc29bf5435335123f151d766880f89f276e
PyRPy <likedatascience@gmail.com>
2019-12-02 17:38:15 -0600

    how to find multiple modes in a list of numbers

--- MathPy/03_stats.py
+++ MathPy/03_stats.py
@@ -28,6 +28,20 @@ def calculate_median(numbers):
 
     return median
 
+# caculate the mode
+from collections import Counter
+def calculate_mode(numbers):
+    c = Counter(numbers)
+    numbers_freq = c.most_common()
+    max_count = numbers_freq[0][1]
+    modes = []
+    for num in numbers_freq:
+        if num[1] == max_count:
+            modes.append(num[0])
+    return modes
+
+    # mode = c.most_common(1)
+    # return mode[0][0]
 
 if __name__ == '__main__':
     donations = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
@@ -37,3 +51,10 @@ if __name__ == '__main__':
 
     median = calculate_median(donations)
     print('Median donation for the last {0} days is {1}'.format(N, median))
+
+    # scores = [7, 8, 9, 2, 10, 9, 9, 9, 9, 4, 5, 6, 1, 5, 6, 7, 8, 6, 1, 10]
+    scores = [5,5,5,4,4,4,9,1,3]
+    modes = calculate_mode(scores)
+    print('The mode(s) of the list of scores are:')
+    for mode in modes:
+        print(mode)
