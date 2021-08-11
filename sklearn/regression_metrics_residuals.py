ade6c10690de1ca749d464e4c2e79edceb096f58
PyRPy <44626163+PyRPy@users.noreply.github.com>
2021-08-11 17:57:44 -0400

    R2 and MSE, to add residuals plot

---
+++ sklearn/regression_metrics_residuals.py
@@ -0,0 +1,68 @@
+# https://github.com/mattharrison/ml_pocket_reference
+# https://scikit-learn.org/stable/modules/cross_validation.html
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn import preprocessing
+from sklearn import metrics 
+
+# remove warnings
+import warnings
+warnings.filterwarnings("ignore")
+
+### --- load data --- ###
+from sklearn.datasets import load_boston
+b = load_boston()
+bos_X = pd.DataFrame(b.data, columns=b.feature_names)
+bos_y = b.target
+bos_X_train, bos_X_test, bos_y_train, bos_y_test = train_test_split(
+    bos_X,
+    bos_y,
+    test_size=0.3,
+    random_state=42,
+)
+bos_sX = preprocessing.StandardScaler().fit_transform(
+    bos_X
+)
+bos_sX_train, bos_sX_test, bos_sy_train, bos_sy_test = train_test_split(
+    bos_sX,
+    bos_y,
+    test_size=0.3,
+    random_state=42,
+)
+
+# Baseline model
+from sklearn.dummy import DummyRegressor
+from sklearn.linear_model import LinearRegression
+br = DummyRegressor()
+br.fit(bos_X_train, bos_y_train)
+br.score(bos_X_test, bos_y_test)  # accuracy
+print("baselne model: ", br.score(bos_X_test, bos_y_test))
+
+lr = LinearRegression()
+lr.fit(bos_X_train, bos_y_train)
+lr.score(bos_X_test, bos_y_test)  # accuracy
+print("linear model: ", lr.score(bos_X_test, bos_y_test))
+
+
+# use random forest as a 'new' regression model
+from sklearn import model_selection
+from sklearn.ensemble import RandomForestRegressor
+
+rfr = RandomForestRegressor(random_state=42, n_estimators=100)
+rfr.fit(bos_X_train, bos_y_train)
+
+bos_y_test_pred = rfr.predict(bos_X_test)
+
+# R2
+print("Random Forest for regression: ")
+print("R2: ", metrics.r2_score(bos_y_test, bos_y_test_pred))
+
+print("Mean squared error: ", metrics.mean_squared_error(bos_y_test, bos_y_test_pred))
+
+# Residuals plot
+
+
+
+
+
